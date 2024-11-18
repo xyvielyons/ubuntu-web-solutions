78cd2da9a7dec8ad6dae4a7f13c874de73a54cc6
xyvie lyons <xyvielyons@gmail.com>
2024-11-18 23:31:00 +0300

    first commit

---
+++ app/providers.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react'
+import {NextUIProvider} from '@nextui-org/react'
+
+
+const Providers = ({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) => {
+  return (
+        <NextUIProvider>{children}</NextUIProvider>
+    
+  )
+}
+
+export default Providers
