60959939f4f7757fb15e6e80897ab3b87808b015
xyvie lyons <xyvielyons@gmail.com>
2024-11-19 22:56:35 +0300

    Portfolio section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Partners from "@/components/shared/Partners";
 import About from "@/components/shared/About";
 import ServicesSection from "@/components/shared/ServicesSection";
 import Footer from "@/components/shared/Footer";
+import Portfolio from "@/components/shared/Portfolio";
 export default function Home() {
   return (
     <div className="dark:bg-black bg-background h-full max-w-screen-xl mx-auto space-y-2 md:space-y-4">
@@ -12,6 +13,7 @@ export default function Home() {
       <Partners></Partners>
       <About></About>
       <ServicesSection></ServicesSection>
+      <Portfolio></Portfolio>
       <Footer></Footer>
     </div>
   );
---
+++ components/shared/Portfolio.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+import { PortfolioLinks } from '@/constants/PorfolioLinks'
+import PortfolioCards from '../cards/PortfolioCards'
+const Portfolio = () => {
+  return (
+    <div className='py-[32px] px-[16px]'>
+        <div className="">
+            <div className="space-y-2">
+                <h1 className='text-[35px] md:text-[40px] font-bold text-gray-800 dark:text-white'>OUR PORTFOLIO</h1>
+                <p className='mysubheading text-[16px]'>“See How We Bring Visions to Life.” </p>
+                <p className='text-[17px] text-gray-600 dark:text-gray-200'>Check out our work and see how we've helped businesses like yours achieve stunning, high-impact websites. </p>
+                <div className="pt-2">
+                    <Image src="/arrowportfolio.svg" alt="portfolio" width={100} height={100} className='dark:filter dark:brightness-50 dark:invert'></Image>
+                </div>
+            </div>
+        </div>
+        <div className="py-4">
+            <PortfolioCards LinkedInIframes={PortfolioLinks}></PortfolioCards>
+        </div>
+    </div>
+  )
+}
+
+export default Portfolio
